4239d05f19fbe98265ea612536de48235e566807
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-04-24 12:30:06 -0300

    iniciando a estrutura

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const flash = require('express-flash');
+
+const app = express();
+
+const conn = require('./db/conn');
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+conn 
+    .sync()
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
