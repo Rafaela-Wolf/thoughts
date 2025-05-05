6ec00e99092d10fe130fa653e00eeedec29fac02
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-05-05 16:40:06 -0300

    fazendo o formulário de criação de pensamento

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -2,4 +2,12 @@ module.exports = class ThoughtController {
     static async showThoughts(req, res) {
         res.render('thoughts/home')
     }
+
+    static async dashboard(req, res) {
+        res.render('thoughts/dashboard')
+    }
+
+    static createThought(req, res) {
+        res.render('thoughts/create')
+    }
 }
