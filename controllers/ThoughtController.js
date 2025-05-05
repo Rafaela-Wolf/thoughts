5a05f3d06ce7d4706e2b44941d9e438ab7305d28
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-05-05 16:57:19 -0300

    inserindo pensamentos no banco de dados

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -10,4 +10,24 @@ module.exports = class ThoughtController {
     static createThought(req, res) {
         res.render('thoughts/create')
     }
+
+    static async createThoughtSave(req, res) {
+
+        const thought = {
+            title: req.body.title,
+            UserId: req.session.userid
+        }
+
+        try {
+            await thought.create(thought)
+
+            req.flash('message', "Pensamento criado com sucesso!")
+
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
